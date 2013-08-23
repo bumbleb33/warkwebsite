967c2cbcbe0e781c935caad0c877521d69b42ea7
gogukt <mohankuldeepis@gmail.com>
2013-08-23 09:32:47 +0000

    CSS back in page, expressjs included

--- web.js
+++ web.js
@@ -9,10 +9,18 @@ var line = fs.readFileSync('index.html','utf8',function(err,data){
 
 var linebuf = new Buffer(line,'utf8');
 
+//Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-    response.send(linebuf.toString());
+  var data = fs.readFileSync('index.html').toString();
+  response.send(data);
 });
+
+
+app.get('/courses', function(request, response) {
+      var data = fs.readFileSync('courses.html').toString();
+      response.send(data);
+    });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
